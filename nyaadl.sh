af9365830e4101f9162fed4810cf8b92bd3c79f2
Georgij Zacharov <gosha.za@gmail.com>
2016-02-16 19:32:14 +0100

    Send correct arguments

--- nyaadl.sh
+++ nyaadl.sh
@@ -3,9 +3,7 @@
 # Download torrents from nyaa. Select using percol, download the
 # torrents with wget.
 
-# TODO: -s might not work
-
-searchnyaa.py "$*" | percol |\
+searchnyaa.py "$@" | percol |\
 while read line; do
     name=`echo "$line" | cut -f 1`
     link=`echo "$line" | cut -f 2`
