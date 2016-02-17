b4ce5b58c65d8d5c8bcbf3a6b88e4b0c089f22d2
Georgij Zacharov <gosha.za@gmail.com>
2016-02-17 18:45:54 +0100

    Add some simple error handling

--- nyaadl.sh
+++ nyaadl.sh
@@ -3,10 +3,26 @@
 # Download torrents from nyaa. Select using percol, download the
 # torrents with wget.
 
-searchnyaa.py "$@" | percol |\
-while read line; do
-    name=`echo "$line" | cut -f 1`
-    link=`echo "$line" | cut -f 2`
+if ! result=$(searchnyaa.py "$@"); then
+    exit 1
+fi
+
+RESULTNUM=$(echo -n "$result" | wc -l)
+echo "RESULTNUM: $RESULTNUM"
+
+if (( $RESULTNUM -eq 0 )); then
+    >&2 echo "no results"
+    exit;
+fi
+
+function download_torrent() {
+    name=`echo "$1" | cut -f 1`
+    link=`echo "$1" | cut -f 2`
 
     wget -O "$name.torrent" "$link"
-done
+}
+
+echo -n "$result" | percol |\
+    while read line; do
+        download_torrent "$line"
+    done
