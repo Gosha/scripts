b9d2f6b0e609978e3b5f3ae4ff46f13681b5d384
Georgij Zacharov <gosha.za@gmail.com>
2016-05-07 20:37:40 +0200

    Tumblr requires https nowadays

--- tumblr_rss.sh
+++ tumblr_rss.sh
@@ -2,7 +2,7 @@
 # Makes a list of every tumblr blog rss I'm following
 # Uses cookies.txt exported from firefox
 
-curl -s -b cookies.txt http://www.tumblr.com/following > ~/.following.html
+curl -s -b cookies.txt https://www.tumblr.com/following > ~/.following.html
 grep -P 'class="avatar"' ~/.following.html | sed 's/></>\n</g' | grep -Po 'http://.*\/(?=")' | while read line
 do
     echo ${line}rss;
